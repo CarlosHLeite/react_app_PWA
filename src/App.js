a69f56c4e5891c64235ce3f2492c05fcbb0c7d3b
Your Nameclear <you@example.com>
2022-09-25 00:22:13 -0300

    Project fineshed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React, { useEffect, useState } from 'react';
+
 import './App.css';
+import video from './videos/sculputre.mp4'
 
 function App() {
 
-  const [data, setData ] = useState();
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("https://orangevalleycaa.org/api/videos");
-      const response = await res.json();
-
-      setData(response)
-    }
-
-    fetchData()
-  }, [])
-
-
-
+ 
   return (
     <div className="App">
       <header>
@@ -26,12 +12,7 @@ function App() {
       </header>
 
       <div>
-        { data.map( video => (
-          <div key={video.id}>
-            <h3>{video.name}</h3>
-            <video height={200} width={300} controls src={video.video_url} />
-          </div>
-        ))}
+        <video controls src={video}  height={200} width={300} />
       </div>
 
     </div>
