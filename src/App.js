7096c4ec24804a360dd536ad6d0f8ca2f6a3b721
Your Nameclear <you@example.com>
2022-09-24 23:31:19 -0300

    Video Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData ] = useState();
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://orangevalleycaa.org/api/videos");
+      const response = await res.json();
+
+      setData(response)
+    }
+
+    fetchData()
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Videos</h1>
       </header>
+
+      <div>
+        { data.map( video => (
+          <div key={video.id}>
+            <h3>{video.name}</h3>
+            <video height={200} width={300} controls src={video.video_url} />
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
